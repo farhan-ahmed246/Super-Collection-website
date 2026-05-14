36be7b3049826e1b3be092833d0e34e01d60dc9a
farhan-ahmed246 <fmukhtar420@gmail.com>
2026-05-14 21:40:18 +0500

    Fix Firebase configuration details
    Updated Firebase configuration with correct authDomain and storageBucket.

--- firebase.js
+++ firebase.js
@@ -3,9 +3,9 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB36u8iB6Y6Uu0tHn_fL6m4A9pQ3G0XgUo",
-  authDomain: "super-collection-website.firebaseapp.com",
-  projectId: "super-collection-55c42",
-  storageBucket: "super-collection-website.appspot.com",
+  authDomain: "super-collection-55c42.firebaseapp.com", // <-- ID check karein
+  projectId: "super-collection-55c42", // <--- Ye wali ID honi chahiye
+  storageBucket: "super-collection-55c42.appspot.com",
   messagingSenderId: "374523675037",
   appId: "1:374523675037:web:7573f5a11f23b7e770732d"
 };
