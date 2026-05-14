e6a2d6659e5fa42724e7f512b6cff862a58d47f8
farhan-ahmed246 <fmukhtar420@gmail.com>
2026-05-14 20:52:22 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyB36u8iB6Y6Uu0tHn_fL6m4A9pQ3G0XgUo",
   authDomain: "super-collection-website.firebaseapp.com",
   projectId: "super-collection-website",
   storageBucket: "super-collection-website.appspot.com",
-  messagingSenderId: "37452367....",
-  appId: "1:37452367....:web:...."
+  messagingSenderId: "374523675037",
+  appId: "1:374523675037:web:7573f5a11f23b7e770732d"
 };
 
 const app = initializeApp(firebaseConfig);
