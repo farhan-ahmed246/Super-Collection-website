911c2aeacd0962d89e464413fa7c346aa502c367
farhan-ahmed246 <fmukhtar420@gmail.com>
2026-05-17 20:20:08 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Super-Collection-website/',  // apna repo name
+  base: '/Super-Collection-website/',
 })
